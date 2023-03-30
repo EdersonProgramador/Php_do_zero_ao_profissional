2fafb4bb15fe93cccdeae52dcfcee5ba24bfb260
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-30 02:47:50 -0300

    Finalizado a aula de Verificadores

---
+++ 1-Fundamentos-da-Programacao/1.Como-funciona-a-programacao.php
@@ -0,0 +1,23 @@
+// A teoria de lógica de programação é igual a prática.
+
+Algorítimo para trocar o pinel 
+
+SE o peneu estiver furado:
+    1 - Pegar as ferramentas.
+    2 - Inserir macaco em baixo do carro.
+    3 - Levantar macaco levemente.
+    4 - Roscar parafusos do pneu.
+    5 - Levantar o macaco até a roda sair do chão.
+    6 - retirar os parafusos.
+    7 - Retirar a roda.
+    8 - Insere a nova roda.
+    9 - Abaixa o macaco levemente.
+    10 - Insere os parafusos.
+    11 - Aperta os parafusos.
+    12 - Retira o macaco.
+    13 - Re-aperta os parafusos.
+CASO CONTRÁRIO:
+    Dirigir até o destino.
+
+
+Pesquisar e se aprofundar em Lógica de Programação.
---
+++ 1-Fundamentos-da-Programacao/2.Variáveis.php
@@ -0,0 +1,9 @@
+nome = Ederson
+idade = 90
+caracteristicas = alegre, feliz, carinhoso.
+
+mostrar na tela:
+    Eu me chamo NOME e tenho IDADE anos.
+    CARACTERISTICAS 2.
+
+    Eu me chamo Ederson e tenho 90 anos e sou feliz.

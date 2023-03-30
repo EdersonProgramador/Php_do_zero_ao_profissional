92815739d94f12b5103419c564ba41caaa0959ab
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-30 03:11:38 -0300

    Iniciando aula de função

--- 1-Fundamentos-da-Programacao/4.Repetidores.php
+++ 1-Fundamentos-da-Programacao/4.Repetidores.php
@@ -1,4 +1,4 @@
-X  = 1
+X = 1
 <
 >
 =
@@ -8,3 +8,12 @@ X  = 1
 ENQUANTO x <= 10
     MOSTRAR NA TELA: x
     X = X + 1
+
+    Estruturas de repetição são recursos que permitem executar trechos de código mais de uma vez de acordo com uma condição. 
+    O PHP possui quatro estruturas de repetição: for, foreach, while e do-while.
+
+<?php
+    for;
+    foreach; 
+    while e 
+    do-while.

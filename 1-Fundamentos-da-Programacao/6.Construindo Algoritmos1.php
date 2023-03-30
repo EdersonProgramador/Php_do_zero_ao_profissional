b6510f782a2aeb831c5c3e4668e5d3dc2c5a247c
Ederson Programador <edersonprogramadorfullstack@outlook.com>
2023-03-30 17:52:02 -0300

    Finalizado construindo algoritimos1

---
+++ 1-Fundamentos-da-Programacao/6.Construindo Algoritmos1.php
@@ -0,0 +1,14 @@
+ALGORITIMO: PEGAR ONIBUS
+
+ir até a parada
+ENQUANTO ônibus não chega
+    esperar ônibus
+subir no ônibus
+pagar passagem
+SE não há passagem
+    pegar dinheiro
+pagar ao cobrador
+troco = dinheiro - valor da passagem
+ENQUANTO banco não está vazio
+    IR PARA O PRÓXIMO
+sentar

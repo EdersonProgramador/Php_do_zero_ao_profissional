758140d25e0a9c82437084e84751879267a7292e
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-19 00:45:07 -0300

    Jogando informações na tela

---
+++ 1-Fundamentos-da-Programacao/12-Jogando_informações_na_tela.php
@@ -0,0 +1,12 @@
+<?php
+    $nome = "Ederson";
+    $sobrenome = "Samuel";
+    $idade = 90;
+
+    echo "Meu nome é: ";
+    echo $nome;
+    echo " ".$sobrenome;
+    echo " e eu tenho ";
+    echo $idade;
+    echo " anos"
+?>

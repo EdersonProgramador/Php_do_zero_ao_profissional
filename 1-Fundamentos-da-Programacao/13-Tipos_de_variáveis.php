138d2a224909e1a17a206bd8dfa6ba381fce89f5
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-19 00:55:52 -0300

    Variáveis compostas

---
+++ 1-Fundamentos-da-Programacao/13-Tipos_de_variáveis.php
@@ -0,0 +1,13 @@
+<?php
+$nome = "Ederson"; // string
+$idade = 90; // inteiro, int, integer
+$nota = 7.5; // float
+$ligado = true; // boolean. TRUE / FALSE
+$caracteristicas = array("alegre", "feliz"); // array
+
+$nome = "1";
+$numero + 2;
+
+$nome = 90;
+$nome + 2;
+?>

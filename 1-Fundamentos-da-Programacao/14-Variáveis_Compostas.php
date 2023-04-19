356986ffc691fafcece0168a76e64146b7ad7869
EdersonProgramador <edersonprogramadorfullstack@outlook.com>
2023-04-19 01:12:29 -0300

    Variaveis compostas 2

---
+++ 1-Fundamentos-da-Programacao/14-Variáveis_Compostas.php
@@ -0,0 +1,23 @@
+<?php
+$produto = array(
+    0 => array(
+        "nome" => "",
+        "quantidade" => "",
+        "infor" => "",
+    ),
+    1 => array(
+        "nome" => "",
+        "quantidade" => "",
+        "infor" => ""
+    )
+);
+
+$produtos[] = array(
+    "nome" => "3",
+    "quantidade" => "",
+    "infor" => ""
+);
+
+$produtos[] = "Teste";
+
+print_r($produtos);
